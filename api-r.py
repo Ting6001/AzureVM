64b58ff8a373929123471f489ff73e23891e9120
YaTing Chang <nangnt1040613@gmail.com>
2021-06-15 10:38:51 +0800

    add return dict

--- api-r.py
+++ api-r.py
@@ -40,8 +40,29 @@ class User (Resource):
         for item in arg['data']:
             print(type(item),item)
 
+    dic = {'230000':[], '23R000':[]}
+        
+    dic_DivInfo = { '230000':{  'DEP':['230220', '230R30'],
+                                'SUB':['ME']
+                            }, 
+                    '23R000':{  'DEP':['23R000', '23R200', '23R300'],
+                                'SUB':['ME', 'HW']
+                        }}
+    for div, data in dic_DivInfo.items():
+        for key, lst in data.items():
+            for item in lst:
+                print(div, key, item)
+                dic_tmp = {}
+                dic_tmp['div'] = div
+                dic_tmp['type'] = key
+                dic_tmp['title'] = item
+                dic_tmp['HC'] = 0
+                for i in range(1,4):
+                    dic_tmp['b_'+str(i)] = round(random.uniform(0.8, 1.3),1)
+                    dic_tmp['a_'+str(i)] = round(random.uniform(0.8, 1.3),1)  
+                dic[div].append(dic_tmp)        
         return{
-            'message': 'Insert user success'
+            'data':dic.get('230000', {})}
         }
 
     # def put(self, name): # update
