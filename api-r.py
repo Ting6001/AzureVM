953b378c89e75ff38c2cbc8ca311b0748c168470
YaTing Chang <nangnt1040613@gmail.com>
2021-07-06 11:51:31 +0800

    edit

--- api-r.py
+++ api-r.py
@@ -92,7 +92,10 @@ class WorkRate (Resource):
             err_class = e.__class__.__name__ #取得錯誤類型
             detail = e.args[0] #取得詳細內容
             cl, exc, tb = sys.exc_info() #取得Call Stack
-            lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            lst_call_stack = traceback.extract_tb(tb)
+            print('@', traceback.format_exc())
+            print('@', lst_call_stack)
+            lastCallStack = lst_call_stack[-1] #取得Call Stack的最後一筆資料
 
             fileName = lastCallStack[0] #取得發生的檔案名稱
             lineNum = lastCallStack[1] #取得發生的行號
