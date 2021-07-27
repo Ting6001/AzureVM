6d63fb6df866e24bea80243cf3c789a089d38236
YaTing Chang <nangnt1040613@gmail.com>
2021-07-27 09:46:30 +0800

    R function v6

--- api-r.py
+++ api-r.py
@@ -25,90 +25,97 @@ class WorkRate (Resource):
     parser = parser.add_argument('data_HC', type=dict, location='json', action="append", help='data Hc info is required')
 
     def post(self): # create
-        start = time.process_time()
-        arg = self.parser.parse_args()
-        print(arg)     
-        set_dep = set()
-        set_sub = set()
-        df_prj = pd.DataFrame()
-        df_HC = pd.DataFrame()
-
-        div = arg['division']
-        if arg['data_Prj']:
-            df_prj = pd.DataFrame(arg['data_Prj'])
-            print('get data_Prj')
-            print(arg['data_Prj'])
-            print(df_prj)
-            # pandas DataFrame去除na，有na值的話，直接轉換會error
-            df_prj.fillna("", inplace=True)
-            df_prj = df_prj.astype({'project_code':str, 'project_code_old':str})
-        if arg['data_HC']:   
-            df_HC = pd.DataFrame(arg['data_HC'])
-            print('get data_HC')
-            print(arg['data_HC'])
-            print(df_HC)
-            # pandas DataFrame去除na，有na值的話，直接轉換會error
-            df_HC = df_HC.drop(['save_time', 'user_id'], axis=1)
-            df_HC = df_HC.astype({'div':str, 'deptid':str, 'project_code':str})
         try:
-            print('div:', div)
-            #################  先把code放進來 debug 用 ####################################################
-            # robjects.r.source("./powerApp_func_v11.R")
-            robjects.r.source("./powerApp_func_multi_v5.r")
-            # function_r = robjects.globalenv['hr_cal']
-            function_r = robjects.globalenv['hr_cal_multi']
+            start = time.process_time()
+            arg = self.parser.parse_args()
+            print(arg)     
+            set_dep = set()
+            set_sub = set()
+            df_prj = pd.DataFrame()
+            df_HC = pd.DataFrame()
 
-            # === 將 Pandas.df 轉換成 R df ===
-            print('==== Transfer Pandas.df to R.df ====')
-            with localconverter(robjects.default_converter + pandas2ri.converter):
-                df_prj_r = robjects.conversion.py2rpy(df_prj)
-                df_HC_r = robjects.conversion.py2rpy(df_HC)
-    
-            # === 呼叫 R Function， return R df ===
-            print('==== Before R Functioin ====')
-            time_R_start = time.process_time()
+            div = arg['division']
+            if arg['data_Prj']:
+                df_prj = pd.DataFrame(arg['data_Prj'])
+                print('get data_Prj')
+                print(arg['data_Prj'])
+                print(df_prj)
+                # pandas DataFrame去除na，有na值的話，直接轉換會error
+                df_prj.fillna("", inplace=True)
+                df_prj = df_prj.astype({'project_code':str, 'project_code_old':str})
+            if arg['data_HC']:   
+                df_HC = pd.DataFrame(arg['data_HC'])
+                print('get data_HC')
+                print(arg['data_HC'])
+                print(df_HC)
+                # pandas DataFrame去除na，有na值的話，直接轉換會error
+                df_HC = df_HC.drop(['save_time', 'user_id'], axis=1)
+                df_HC = df_HC.astype({'div':str, 'deptid':str, 'project_code':str})
+            try:
+                print('div:', div)
+                #################  先把code放進來 debug 用 ####################################################
+                robjects.r.source("./powerApp_func_multi_v6.R")
+                function_r = robjects.globalenv['hr_cal_multi']
 
-            df_result_r = function_r(df_prj_r, df_HC_r, div)
+                # === 將 Pandas.df 轉換成 R df ===
+                print('==== Transfer Pandas.df to R.df ====')
+                with localconverter(robjects.default_converter + pandas2ri.converter):
+                    df_prj_r = robjects.conversion.py2rpy(df_prj)
+                    df_HC_r = robjects.conversion.py2rpy(df_HC)
+        
+                # === 呼叫 R Function， return R df ===
+                print('==== Before R Functioin ====')
+                time_R_start = time.process_time()
 
-            time_taken_R = round(time.process_time() - time_R_start,3)
-            print('==== Transfer R.df to Pandas.df ====')
-            # === 將 R df 轉換成  Pandas.df ===
-            with localconverter(robjects.default_converter + pandas2ri.converter):
-                df_result = robjects.conversion.rpy2py(df_result_r)       
-            print('======== Function return ==========')
-            dic_result = {}
-            if isinstance(df_result, pd.DataFrame):
-                df_result.fillna(0.0, inplace=True)
-                dic_result = df_result.to_dict('records')
-                print(dic_result)
-            time_taken = round(time.process_time() - start,3)
-            print('Totally Take:', time_taken, 's,  R fuction:', time_taken_R, 's')
-            print('====================================================================================')
-            return dic_result
+                df_result_r = function_r(df_prj_r, df_HC_r, div)
+
+                time_taken_R = round(time.process_time() - time_R_start,3)
+                print('==== Transfer R.df to Pandas.df ====')
+                # === 將 R df 轉換成  Pandas.df ===
+                with localconverter(robjects.default_converter + pandas2ri.converter):
+                    df_result = robjects.conversion.rpy2py(df_result_r)       
+                print('======== Function return ==========')
+                dic_result = {}
+                if isinstance(df_result, pd.DataFrame):
+                    df_result.fillna(0.0, inplace=True)
+                    dic_result = df_result.to_dict('records')
+                    print(dic_result)
+                time_taken = round(time.process_time() - start,3)
+                print('Totally Take:', time_taken, 's,  R fuction:', time_taken_R, 's')
+                print('====================================================================================')
+                return dic_result
+            except Exception as e:
+                self.save_log(e, arg)
         except Exception as e:
-            query = 'INSERT INTO LogInfo (user_input, error_msg, save_time, user_id) VALUES (?,?,?,?)'
+            self.save_log(e, None)
+
+
+    def save_log(self, e, arg):
+        query = 'INSERT INTO LogInfo (user_input, error_msg, save_time, user_id) VALUES (?,?,?,?)'
+        user_id, user_input = '', ''
+        if arg != None:
             user_id = arg['user']
             user_input = str(arg)
-            save_time = datetime.now()
-            
-            err_class = e.__class__.__name__ #取得錯誤類型
-            detail = e.args[0] #取得詳細內容
-            cl, exc, tb = sys.exc_info() #取得Call Stack
-            lst_call_stack = traceback.extract_tb(tb)
-            err_msg = '【{}】 {}'.format(err_class, detail)
-            for CallStack in lst_call_stack:
-                fileName = CallStack[0] #取得發生的檔案名稱
-                lineNum = CallStack[1] #取得發生的行號
-                funcName = CallStack[2] #取得發生的函數名稱
-                err_msg += "\nFile \"{}\", line {}, in {}".format(fileName, lineNum, funcName)
+        save_time = datetime.now()
+        
+        err_class = e.__class__.__name__ #取得錯誤類型
+        detail = e.args[0] #取得詳細內容
+        cl, exc, tb = sys.exc_info() #取得Call Stack
+        lst_call_stack = traceback.extract_tb(tb)
+        err_msg = '【{}】 {}'.format(err_class, detail)
+        for CallStack in lst_call_stack:
+            fileName = CallStack[0] #取得發生的檔案名稱
+            lineNum = CallStack[1] #取得發生的行號
+            funcName = CallStack[2] #取得發生的函數名稱
+            err_msg += "\nFile \"{}\", line {}, in {}".format(fileName, lineNum, funcName)
 
-            print(err_msg)
-            data = (user_input, err_msg, save_time, user_id)
+        print(err_msg)
+        data = (user_input, err_msg, save_time, user_id)
 
-            conn, cursor = self.connect_db()
-            cursor.execute(query, data)
-            conn.commit()
-            self.close_db(conn, cursor)
+        conn, cursor = self.connect_db()
+        cursor.execute(query, data)
+        conn.commit()
+        self.close_db(conn, cursor)
 
         ##############################################################################################
         # 假資料區
